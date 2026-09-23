a1df326e4d4b376cace0667265fb2419a735de67
vaniamariabatista-cyber <vaniamariabatista@gmail.com>
2026-09-23 20:24:30 -0300

    Adiciona exercício par ou ímpar

---
+++ par_impar.py
@@ -0,0 +1,13 @@
+while True:
+    numero = int (input("digite um número: "))
+
+    if numero == 0:
+        print ("Fim do programa")
+        break
+        
+        
+ 
+    if numero % 2 == 0:
+        print ("Par")
+    else:
+        print ("Ímpar")
